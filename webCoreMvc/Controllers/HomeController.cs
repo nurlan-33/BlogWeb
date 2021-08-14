109492f41f05c5eda5365def3bb92f9e1f5aa060
quluzadanurlan13@gmail.com <quluzadanurlan13@gmail.com>
2021-08-14 20:46:47 +0400

    blog site first commit

---
+++ webCoreMvc/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using webCoreMvc.Models;
+using System.Data.SqlClient;
+
+namespace webCoreMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        Parametrs db=new Parametrs();
+        public IActionResult Index()
+        {
+            db.number1=9;
+            db.number2=4;
+            ViewBag.deg="Hello world";   
+            return View(db);
+        }
+        
+    }
+}
---
+++ webCoreMvc/Models/Parametrs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webCoreMvc.Models
+{
+        public class Parametrs
+    {
+        public int number1 { get; set; }
+        public int number2 { get; set; }
+    }
+}
